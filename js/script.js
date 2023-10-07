85750c7b6db17ec5958081b5f1a8f976d9438b4f
Jeremys11 <jeremysingh116@gmail.com>
2023-10-07 16:28:36 -0400

    map renders
    no markers or data lookup

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var message = "hi"; //javascript untyped
+
+function a () {
+    //empty
+}
+
+var b = function () {/*empty*/} //This assigns var to function, not result
+
+function compare(x,y){
+    return x > y;
+}
+
+var c = compare(4,2);
+
+compare(4,5);
