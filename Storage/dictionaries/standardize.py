36bf179c74c1dabe9e85e9b8caf5cee5ec21b0bb
Jeremy Singh <jeremys1199@gmail.com>
2023-10-09 16:02:21 -0400

    Finally working locally

--- Storage/dictionaries/standardize.py
+++ Storage/dictionaries/standardize.py
@@ -67,8 +67,23 @@ def createCombined():
                 writer.writerow(mylist)
 
 def csvTojson():
-    df = pd.read_csv ("Storage/dictionaries/Creole_Languages.csv")
-    df.to_json ("Storage/dictionaries/Creole_Languages.json")
+    jsonArray = []
+      
+    #read csv file
+    with open("Storage/dictionaries/Creole_Languages.csv", encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open("Storage/dictionaries/Creole_Languages.json", 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+
 
 
 csvTojson()
